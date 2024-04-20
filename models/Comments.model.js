46927ba5948c4aa55a98897eb97b054650d0d209
adityach0uhan <m4with6x@gmail.com>
2024-04-20 22:58:21 +0530

    Created a Comment Model

---
+++ models/Comments.model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const Comments = new mongoose.Schema({
+  videoId: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
