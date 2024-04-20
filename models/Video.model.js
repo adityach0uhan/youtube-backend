22ed94857b5432532fb61862cd2364a7343f15ea
adityach0uhan <m4with6x@gmail.com>
2024-04-20 22:55:20 +0530

    Created a Video Model

--- models/Video.model.js
+++ models/Video.model.js
@@ -1,43 +1,44 @@
 import mongoose from "mongoose";
 
-const VideoSchema = new mongoose.Schema({
-  userId: {
-    type: String,
-    required: true,
+const VideoSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      default: [],
+    },
+    thumbnail: {
+      type: String,
+      default: [],
+    },
+    videoURL: {
+      type: String,
+      default: [],
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    likes: {
+      type: Number,
+      default: 0,
+    },
+    Dislikes: {
+      type: Number,
+      default: 0,
+    },
+    tags: {
+      type: String,
+      required: true,
+    },
   },
-  title: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  thumbnail: {
-    type: String,
-    required: true,
-  },
-  videoURL: {
-    type: String,
-    required: true,
-  },
-  views: {
-    type: Number,
-    default: 0,
-    required: true,
-  },
-  likes: {
-    type: Number,
-    default: 0,
-    required: true,
-  },
-  Dislikes: {
-    type: Number,
-    default: 0,
-    required: true,
-  },
-  tags: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
+export default mongoose.model("Video", VideoSchema);
