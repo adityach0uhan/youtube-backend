8416ebe6d9a1c878653b2b00c23a395d91bfb2e7
adityach0uhan <m4with6x@gmail.com>
2024-04-21 14:34:33 +0530

    Added Signup route and Controller

---
+++ controllers/signup.controller.js
@@ -0,0 +1,5 @@
+const signupController = (req,res) => {
+    res.send("Signup Route Hit")
+}
+
+export default signupController;
--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
-import ConnectDatabase from './DataBase/db.js'
-dotenv.config();
-import userRoute from './routes/user.route.js'
-import commentRoute from "./routes/comment.route.js";
+import ConnectDatabase from "./DataBase/db.js";
 
-const app = express();
+// importing routes
+import userRoute from "./routes/user.route.js";
+import commentRoute from "./routes/comment.route.js";
+import signupRoute from "./routes/signup.route.js";
 const port = process.env.PORT;
 
-app.use('/api/users', userRoute);
-app.use('/api/comment', commentRoute);
+const app = express();
+dotenv.config();
 
+// ROUTES
+app.use("/api/users", userRoute);
+app.use("/api/comment", commentRoute);
+app.use("/api/signup", signupRoute);
 
-app.get('/', (req,res) => {
-    res.send("Home Route")
-})
+app.get("/", (req, res) => {
+  res.send("Home Route");
+});
 
-
-
-app.listen(port, async() => {
-    await ConnectDatabase();
-    console.log(`Server is running on port ${port}`)
-})
+app.listen(port, async () => {
+  await ConnectDatabase();
+  console.log(`Server is running on port ${port}`);
+});
