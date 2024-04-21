2761ca79306967e0d59b917c2e7d9d077ac1608d
adityach0uhan <m4with6x@gmail.com>
2024-04-21 15:11:41 +0530

    Added Bcryptjs

--- controllers/signup.controller.js
+++ controllers/signup.controller.js
@@ -1,15 +1,20 @@
 import userModel from "../models/User.model.js";
+import bcryptjs from "bcryptjs";
+
 const signupController = async (req, res) => {
   try {
     const { username, email, password } = await req.body;
+    const salt = bcryptjs.genSaltSync(10);
+    const hashedPassword =  bcryptjs.hashSync(password, salt);
+
     await userModel.create({
       username,
       email,
-      password,
+      password:hashedPassword,
     });
     res.send(req.body);
   } catch (error) {
-    console.log("Error in Signup Controller",error);
+    console.log("Error in Signup Controller", error);
   }
 };
 
