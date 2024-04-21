df65b4681d73c2086ebfd38347cddf9e072c62b6
adityach0uhan <m4with6x@gmail.com>
2024-04-21 14:50:16 +0530

    Added Middlewares to accept json data

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import ConnectDatabase from "./DataBase/db.js";
-
 // importing routes
 import userRoute from "./routes/user.route.js";
 import commentRoute from "./routes/comment.route.js";
 import signupRoute from "./routes/signup.route.js";
 import loginRoute from './routes/login.route.js'
 
-
 const port = process.env.PORT;
 const app = express();
 dotenv.config();
+// Middleware to parse JSON bodies
+app.use(express.json())
+// Middleware to parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
 
-// ROUTES
+
 app.use("/api/users", userRoute);
 app.use("/api/comment", commentRoute);
 app.use("/api/login", loginRoute);
