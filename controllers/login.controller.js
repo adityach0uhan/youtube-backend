b78cd3f0f2186a873885da2f6c9882f8c1dc27b4
adityach0uhan <m4with6x@gmail.com>
2024-04-28 12:48:21 +0530

    removed password from response

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,27 +1,24 @@
 import userModel from "../models/User.model.js";
 import bcryptjs from "bcryptjs";
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 const loginController = async (req, res) => {
   try {
-    const { email, password } = await req.body;
+    const { email, userpassword } = await req.body;
     const userdetails = await userModel.findOne({ email });
-      const verified = await bcryptjs.compare(password, userdetails.password);
-      
-      if (!verified) {
-            return res.status(401).send("Wrong Credentials");
-      }
-    //   jwt.sign(userdetails)
-      const JWTtoken=jwt.sign({id:userdetails._id}, process.env.JWT_SECRET);
-      
-      res.cookie(
-          "access_token", JWTtoken, {
-             httpOnly:true
-         }
-      ).status(200).json({
-         message:"cookie set"
-     })
-
-
+    const verified = await bcryptjs.compare(userpassword,userdetails.password);
+    if (!verified) {
+      return res.status(401).send("Wrong Credentials");
+    }
+    const JWTtoken = await jwt.sign({ id: userdetails._id },process.env.JWT_SECRET);
+    const { password, ...otherDetails } = userdetails._doc;
+    res
+      .cookie("access_token", JWTtoken, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json({
+        message: otherDetails,
+      });
   } catch (error) {
     console.log(error);
   }
