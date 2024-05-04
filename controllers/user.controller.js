b22b9802a473fc434130f0a01fed161db83a11ee
adityach0uhan <m4with6x@gmail.com>
2024-05-04 12:23:20 +0530

    Update Controller Complete

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,11 +1,26 @@
+import userModel from "../models/User.model.js";
+
 export const userInfo = (req, res) => {
   const requestBody = req.body;
   console.log("get User route ");
 };
 
-export const updateUser = (req, res, next) => {
-  res.send("updateUser user");
-  next()
+// update user controller
+export const updateUser = async (req, res, next) => {
+  if (req.params.id == req.user.id) {
+    try {
+      const updated = await userModel.findByIdAndUpdate(
+        req.params.id,
+        { $set: req.body },
+        { new: true }
+      );
+      res.status(200).json(updated);
+    } catch (error) {
+      next("Database Error", 400, "Bad request");
+    }
+  } else {
+    return next("Authentication", 403, "Unauthorized access");
+  }
 };
 
 export const getUser = (req, res, next) => {
