df71810214fea85d1996788da7195509592aa183
adityach0uhan <m4with6x@gmail.com>
2024-05-18 22:58:03 +0530

    Added updateVideo controller

--- controllers/video.controller.js
+++ controllers/video.controller.js
@@ -1,4 +1,5 @@
 import videoModel from "../models/Video.model.js";
+import { createError } from "../errors.js";
 export const getVideo = async (req, res, next) => {
   try {
   } catch (error) {
@@ -10,7 +11,7 @@ export const addVideo = async (req, res, next) => {
   try {
     const newVideo = new videoModel({ userId: req.user.id, ...req.body });
     const saveVideo = await newVideo.save();
-    res.status(200).json(saveVideo)
+    res.status(200).json(saveVideo);
   } catch (error) {
     console.log(error);
   }
@@ -25,7 +26,23 @@ export const deleteVideo = async (req, res, next) => {
 
 export const updateVideo = async (req, res, next) => {
   try {
+    const video = videoModel.findById(req.params.id);
+    if (!video) {
+      return next(createError("Video", 404, "Video Not Found"));
+    }
+    if (video.userId === req.user.id) {
+      const updatedVideo = videoModel.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        {
+          new: true,
+        }
+      );
+      res.status(200).json(updatedVideo)
+    }
   } catch (error) {
-    console.log(error);
+next(error)
   }
 };
