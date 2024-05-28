67260109ed36a119ae04a1de5efcbc252fe8d8d1
adityach0uhan <m4with6x@gmail.com>
2024-05-28 22:52:35 +0530

    Added deleteComment controller

--- controllers/comment.controller.js
+++ controllers/comment.controller.js
@@ -1,5 +1,5 @@
-import commentsModel from '../models/Comments.model'
-
+import commentsModel from "../models/Comments.model";
+import videoModel from "../models/Video.model";
 export const getComment = async (req, res, next) => {
   try {
     const comments = await commentsModel.find();
@@ -9,13 +9,27 @@ export const getComment = async (req, res, next) => {
   }
 };
 
-
 export const addComment = async (req, res, next) => {
   try {
-    const newComment =  new commentsModel({ ...req.body, userId: req.user.id })
+    const newComment = new commentsModel({ ...req.body, userId: req.user.id });
     const savedComment = await newComment.save();
-    res.status(200).json(savedComment)
+    res.status(200).json(savedComment);
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
+
+export const deleteComment = async (req, res, next) => {
+  try {
+    const comment = await commentsModel.findById(req.params.id);
+    const video = await videoModel.findById(req.params.id);
+    if (req.params.id === comment.userId || req.params.id === video.userId) {
+      await commentsModel.findByIdAndDelete(req.params.id);
+      res.status(200).json("Comment Deleted ")
+    } else {
+      createError("Authentication Error",403,"You can delete only your comment")
+    }
+  } catch (error) {
+    next(error);
+  }
+};
