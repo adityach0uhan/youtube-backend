e241b691378593fe227dbe92915fd9865293485c
adityach0uhan <m4with6x@gmail.com>
2024-05-29 23:03:36 +0530

    started testing of comment apis

--- routes/comment.route.js
+++ routes/comment.route.js
@@ -8,7 +8,7 @@ import { tokenVerification } from "../tokenVerification.js";
 const router = express.Router();
 
 router.get("/", getComment);
-router.post("/:id", tokenVerification, addComment);
+router.post("/", tokenVerification, addComment);
 router.delete("/:videoId", tokenVerification, deleteComment);
 
 export default router;
